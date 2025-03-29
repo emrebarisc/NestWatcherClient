0b6903af8f6831ca9cf1ca44494382e937b9cede
emrebarisc <>
2025-03-29 23:26:06 +0300

    Update network functionality

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,136 +1,100 @@
-
-#include<stdio.h>
-#include<winsock2.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
 #include <iostream>
 
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
+#pragma comment(lib, "ws2_32.lib")
 
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
+#define PORT 1000
 
-#define _WINSOCK_DEPRECATED_NO_WARNINGS
-
-#pragma comment(lib,"ws2_32.lib")
-
-#include <SDL3/SDL.h>
-#include <SDL3/SDL_render.h>
-
-#define BUFLEN 2048
-#define PORT 5005
-int main(int argc, char* argv[])
+enum class Command : unsigned char
 {
+    None = 0,
+    Connect,
+    Disconnect,
+    StartCamera,
+    StopCamera,
+    SetResolution,
+    SetFPS,
+    SetPixelFormat,
+    StartRecording,
+    PauseRecording,
+    StopRecording,
+    TakeAPhotograph,
+    StartStream,
+    StopStream,
+    Shutdown
+};
 
-
-	SOCKET s;
-	struct sockaddr_in server, si_other;
-	int slen, recv_len;
-	char buf[BUFLEN];
-	WSADATA wsa;
-
-	slen = sizeof(si_other);
-
-	printf("\nInitialising Winsock...");
-	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
-	{
-		printf("Failed. Error Code : %d", WSAGetLastError());
-		exit(EXIT_FAILURE);
-	}
-	printf("Initialised.\n");
-
-	if ((s = socket(AF_INET, SOCK_DGRAM, 0)) == INVALID_SOCKET)
-	{
-		printf("Could not create socket : %d", WSAGetLastError());
-	}
-	printf("Socket created.\n");
-
-	server.sin_family = AF_INET;
-	server.sin_addr.s_addr = INADDR_ANY;
-	server.sin_port = htons(PORT);
-
-	if (bind(s, (struct sockaddr*)&server, sizeof(server)) == SOCKET_ERROR)
-	{
-		printf("Bind failed with error code : %d", WSAGetLastError());
-		exit(EXIT_FAILURE);
-	}
-	puts("Bind done");
-
-	const int totalSize = 640 * 480 * 3;
-	unsigned char imageBuffer[totalSize];
-
-	memset(imageBuffer, 0, totalSize);
-
-	if (!SDL_Init(SDL_INIT_VIDEO))
-	{
-		std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
-		return 1;
-	}
-
-	SDL_Window* window = SDL_CreateWindow("SDL3 Window", 800, 600, SDL_WINDOW_RESIZABLE);
-	if (window == nullptr)
+class CommandMessage
 {
-		std::cerr << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
-		SDL_Quit();
-		return 1;
-	}
+public:
+    Command command;
+    char commandMessage[64];
+};
 
-	SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
-	if (renderer == nullptr)
+int main()
 {
-		std::cerr << "Renderer could not be created! SDL_Error: " << SDL_GetError() << std::endl;
-		SDL_DestroyWindow(window);
-		SDL_Quit();
-		return 1;
-	}
+    WSADATA wsaData;
+    int wsaStartupResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (wsaStartupResult != 0)
+    {
+        std::cerr << "WSAStartup failed with error: " << wsaStartupResult << std::endl;
+        return 1;
+    }
 
-	bool quit = false;
+    SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (clientSocket == INVALID_SOCKET)
+    {
+        std::cerr << "Socket creation failed with error: " << WSAGetLastError() << std::endl;
+        WSACleanup();
+        return 1;
+    }
 
-	SDL_Event e;
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(PORT);
 
-	int currentIndex = 0;
+    const char* serverIP = "192.168.31.154";
+    if (InetPton(AF_INET, serverIP, &serverAddress.sin_addr) <= 0)
+    {
+        std::cerr << "Invalid address or address not supported." << std::endl;
+        closesocket(clientSocket);
+        WSACleanup();
+        return 1;
+    }
 
-	while (!quit)
-	{
-		while (SDL_PollEvent(&e) != 0)
-		{
-			if (e.type == SDL_EVENT_QUIT)
-			{
-				quit = true;
-			}
-		}
+    int connectResult = connect(clientSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+    if (connectResult == SOCKET_ERROR)
+    {
+        std::cerr << "Connection failed with error: " << WSAGetLastError() << std::endl;
+        closesocket(clientSocket);
+        WSACleanup();
+        return 1;
+    }
 
-		fflush(stdout);
+    CommandMessage* commandMessage = new CommandMessage();
+    commandMessage->command = Command::TakeAPhotograph;
+    strncpy_s(commandMessage->commandMessage, sizeof(commandMessage->commandMessage), "Hello, Server!", _TRUNCATE);
 
-		if ((recv_len = recvfrom(s, buf, BUFLEN, 0, (struct sockaddr*)&si_other, &slen)) == SOCKET_ERROR)
-		{
-			printf("recvfrom() failed with error code : %d", WSAGetLastError());
-			exit(EXIT_FAILURE);
-		}
+    int sendResult = send(clientSocket, (char*)commandMessage, sizeof(*commandMessage), 0);
+    if (sendResult == SOCKET_ERROR)
+    {
+        std::cerr << "Send failed with error: " << WSAGetLastError() << std::endl;
+    }
+    delete commandMessage;
 
-		memcpy(&imageBuffer[currentIndex * 1920], buf, 1920);
-		++currentIndex;
+    CommandMessage* takeAPhotoCommandMessage = new CommandMessage();
+    takeAPhotoCommandMessage->command = Command::TakeAPhotograph;
+    strncpy_s(takeAPhotoCommandMessage->commandMessage, sizeof(takeAPhotoCommandMessage->commandMessage), "TakeAPhotograph", _TRUNCATE);
+    int photoCommandSendResult = send(clientSocket, (char*)takeAPhotoCommandMessage, sizeof(*takeAPhotoCommandMessage), 0);
+    if (photoCommandSendResult == SOCKET_ERROR)
+    {
+        std::cerr << "Send failed with error: " << WSAGetLastError() << std::endl;
+    }
+    delete takeAPhotoCommandMessage;
 
-		if (currentIndex == 480)
-		{
-			stbi_write_png("C:/Users/Baris/Desktop/cameraImage.png", 640, 480, 3, imageBuffer, 1920);
-			break;
-		}
+    closesocket(clientSocket);
+    WSACleanup();
 
-		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-		SDL_RenderClear(renderer);
-
-		SDL_RenderPresent(renderer);
-	}
-
-	closesocket(s);
-	WSACleanup();
-
-
-	SDL_DestroyRenderer(renderer);
-
-	SDL_DestroyWindow(window);
-
-	SDL_Quit();
-
-	return 0;
+    return 0;
 }
