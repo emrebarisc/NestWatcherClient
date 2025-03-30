6a386ee3debc8c916dbdbcbaa63baa52bf8e0286
emrebarisc <>
2025-03-31 00:21:40 +0300

    Convert NetworkManager from C style to C++ style

--- Source/Network/NetworkInterface.h
+++ Source/Network/NetworkInterface.h
@@ -2,13 +2,14 @@
 
 struct sockaddr;
 
-namespace NetworkInterface
+class NetworkInterface
 {
-	extern int CreateSocket(int af, int type, int protocol);
-	extern int Connect(int socket, sockaddr* socketAddress, int length);
-	extern int Bind(int socket, sockaddr* socketAddress, int length);
-	extern int Close(int socket);
-	extern int Accept(int socketFd, struct sockaddr* address, int* length);
-	extern int Send(int socketFd, const char* buffer, size_t size, int flags);
-	extern int Receive(int socketFd, char* buffer, size_t size, int flags);
-}
+public:
+	static int CreateSocket(int af, int type, int protocol);
+	static int Connect(int socket, sockaddr* socketAddress, int length);
+	static int Bind(int socket, sockaddr* socketAddress, int length);
+	static int Close(int socket);
+	static int Accept(int socketFd, struct sockaddr* address, int* length);
+	static int Send(int socketFd, const char* buffer, size_t size, int flags);
+	static int Receive(int socketFd, char* buffer, size_t size, int flags);
+};
